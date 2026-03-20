888cbac8137468854240b43ad4f147bce7db23f4
joll3a <its.jolla6@gmail.com>
2026-03-20 23:34:42 +0300

    v1.1.1.1.1.1.1.3

--- routes/pricingRoutes.js
+++ routes/pricingRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {  updateMilkRate, updateInventoryCategory, getMilkHistory, getCurrentPrices, getInventoryCategories } = require('../controllers/pricingController');
 
 router.post('/milk',updateMilkRate);
-router.patch('/inventory/:category',updateInventoryCategory);
+router.patch('/inventory/:itemId',updateInventoryCategory);
 router.get('/milk-history',getMilkHistory);
 router.get('/categories',getInventoryCategories);
 router.get('/current',getCurrentPrices);
